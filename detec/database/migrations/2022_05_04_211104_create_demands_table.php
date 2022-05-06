22986f590e8e4f7dfc0017afd4ab577f14422b57
djchristiandf <djchristiandf@gmail.com>
2022-05-06 12:06:30 -0300

    alter migrate table add collumn060520221205

--- detec/database/migrations/2022_05_04_211104_create_demands_table.php
+++ detec/database/migrations/2022_05_04_211104_create_demands_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->date('date_distribution_demand')->nullable();
             $table->date('delivery_demand')->nullable();
             $table->string('address_demand', 100);
-
+            $table->string('description', 250)->nullable();
             //foreign
             $table->foreignId('activity_id')->constrained('activities')->onDelete('cascade');
             $table->foreignId('interested_id')->constrained('interesteds')->onDelete('cascade');
